c7e426fdf499aeb44cec307b5a192557bce38598
Tradin Titans Arena <tradintitansarena@Tradins-Mac-mini.local>
2025-12-14 16:17:57 +0530

    build command

--- server/config/db.js
+++ server/config/db.js
@@ -5,8 +5,8 @@ const connectDB = async () => {
     await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB Connected");
   } catch (err) {
-    console.error("Mongo connection failed");
-    process.exit(1);
+    console.error("MongoDB connection failed:", err.message);
+    // ❌ process.exit hata diya
   }
 };
 
